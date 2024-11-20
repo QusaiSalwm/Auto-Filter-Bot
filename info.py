d5718dee211b447b4c6c6e77cc5e43495815a29e
QusaiSalwm <81245445+QusaiSalwm@users.noreply.github.com>
2024-11-20 15:12:54 +0300

    Update info.py

--- info.py
+++ info.py
@@ -123,11 +123,10 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-
 URL = environ.get("URL", "") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
-   
+    exit()
 else:
     if URL.startswith(('https://', 'http://')):
         if not URL.endswith("/"):
@@ -136,5 +135,6 @@ else:
         URL = f'http://{URL}/'
     else:
         print('Error - URL is not valid, exiting now')
+        exit()
         
 
